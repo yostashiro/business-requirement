32eeea07365e58c29ce80e9f7d6c74b7bf1052e4
Yoshi Tashiro <tashiro@roomsfor.hk>
2016-06-09 08:49:31 -0400

    [IMP] shorten code

--- business_requirement/wizards/mail_compose_message.py
+++ business_requirement/wizards/mail_compose_message.py
@@ -9,12 +9,12 @@ class MailComposeMessage(models.TransientModel):
     _inherit = 'mail.compose.message'
 
     def default_get(self, cr, uid, fields, context=None):
-        res = super(MailComposeMessage, self).default_get(cr, uid, fields, context=context)
-        if context.get('default_model') == 'business.requirement':
-            if context.get('default_res_id'):
-                br_object = self.pool.get(
-                    context.get('default_model')
-                ).browse(cr, uid, context['default_res_id'])
-                res['subject'] = 'Re: %s-%s' % (
-                    br_object.name, br_object.description)
+        res = super(MailComposeMessage, self).default_get(cr, uid,
+            fields, context=context)
+        if context.get('default_model') == 'business.requirement' and \
+            context.get('default_res_id'):
+            br_rec = self.pool.get(
+                context.get('default_model')
+            ).browse(cr, uid, context['default_res_id'])
+            res['subject'] = 'Re: %s-%s' % (br_rec.name, br_rec.description)
         return res
